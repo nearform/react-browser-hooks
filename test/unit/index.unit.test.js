d71dd0f9e7c75e3dc00b97036124420a222f8daf
thomas <thomas.treffry@nearform.com>
2019-01-21 13:13:03 -0800

    Configure unit tests

--- test/unit/index.unit.test.js
+++ test/unit/index.unit.test.js
@@ -2,7 +2,7 @@ import expect from 'expect'
 import React from 'react'
 import {render, unmountComponentAtNode} from 'react-dom'
 
-import FullScreen from '../demo/src/components/fullscreen'
+import FullScreen from '../../demo/src/components/fullscreen'
 
 describe('FullScreen', () => {
   let node
