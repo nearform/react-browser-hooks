cb166d106fafa4360a26c3620fe9f14687559264
Joe Warren <joe.warren@nearform.com>
2019-01-29 16:30:48 +0000

    Add more information to output of useOrientation hook

--- demo/src/components/orientation.js
+++ demo/src/components/orientation.js
@@ -3,13 +3,17 @@ import React from 'react'
 import { useOrientation } from '../../../src'
 
 export default function Resize() {
-  const screenAngle = useOrientation()
+  const { angle, orientationType } = useOrientation()
 
   return (
     <div>
       <h2>Orientation Demo</h2>
-      <p>Screen angle: {screenAngle}&deg;</p>
-      Try using dev tools to rotate your screen, you should see this number
+      <p>
+        Screen angle: {angle}&deg;
+        <br />
+        Orientation type: {orientationType}
+      </p>
+      Try using dev tools to rotate your screen, you should see these values
       change.
     </div>
   )
--- src/hooks/orientation.js
+++ src/hooks/orientation.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 
 export function useOrientation() {
-  const currentAngle =
-    typeof window === 'object' ? window.screen.orientation.angle : 0
-  const [state, setState] = useState(currentAngle)
+  const currentOrientation =
+    typeof window === 'object' ? window.screen.orientation : null
+  const [state, setState] = useState(currentOrientation)
 
   useEffect(() => {
-    const handler = () => setState(window.screen.orientation.angle)
+    const handler = () => setState(window.screen.orientation)
     if (typeof window !== 'object') return () => {}
 
     window.addEventListener('orientationchange', handler)
     return () => window.removeEventListener('orientationchange', handler)
   }, [])
 
-  return state
+  return { angle: state.angle, orientationType: state.type }
 }
