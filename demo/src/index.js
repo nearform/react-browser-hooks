1e520c88fc5f15a20c9f02f1c9606fe32be3dd87
Joe Warren <joe.warren@nearform.com>
2019-01-30 12:04:01 +0000

    Make demo page a functional component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 
 import FullScreen from './components/fullscreen'
 import Resize from './components/resize'
 import Orientation from './components/orientation'
-class Demo extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Reach Browser Hooks Examples</h1>
-        <FullScreen />
-        <Resize />
-        <Orientation />
-      </div>
-    )
-  }
-}
+const Demo = () => (
+  <div>
+    <h1>Reach Browser Hooks Examples</h1>
+    <FullScreen />
+    <Resize />
+    <Orientation />
+  </div>
+)
 
 render(<Demo />, document.querySelector('#demo'))
