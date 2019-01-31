48e08273c16ac75e29132063515f60b684189c18
thomas <thomas.treffry@nearform.com>
2019-01-31 13:50:17 -0800

    Add volume and controls

--- demo/src/components/audio.js
+++ demo/src/components/audio.js
@@ -4,13 +4,22 @@ import { useMediaControls } from '../../../src'
 
 export default function Audio() {
   const player = useRef(null)
-  const { paused, toggle } = useMediaControls(player)
+  const {
+    mute,
+    muted,
+    unmute,
+    paused,
+    setVolume,
+    toggle,
+    volume
+  } = useMediaControls(player)
 
   return (
     <div>
       <h3>Audio</h3>
       <p>
         <audio
+          controls
           ref={player}
           src="https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview118/v4/00/ea/65/00ea65de-25f1-3217-7a2b-097e989dd884/mzaf_5030465069226935473.plus.aac.p.m4a"
           width="250">
@@ -19,8 +28,19 @@ export default function Audio() {
       </p>
       <p>
         <button onClick={toggle}>Play/Pause</button>
+        <button onClick={muted ? unmute : mute}>
+          {muted ? 'Unmute' : 'Mute'}
+        </button>
+        <button onClick={() => setVolume(Math.round((volume - 0.1) * 10) / 10)}>
+          Volume down
+        </button>
+        <button onClick={() => setVolume(Math.round((volume + 0.1) * 10) / 10)}>
+          Volume up
+        </button>
       </p>
-      <p>The audio paused: {paused.toString()}</p>
+      <p>The audio is paused: {paused.toString()}</p>
+      <p>The audio is volume: {volume.toString()}</p>
+      <p>The audio is muted: {muted.toString()}</p>
     </div>
   )
 }
--- src/hooks/media-controls.js
+++ src/hooks/media-controls.js
@@ -1,47 +1,89 @@
 import { useState, useEffect } from 'react'
 
 export function useMediaControls(element) {
+  const [muted, setMuted] = useState(getMuted())
+  const [oldVolume, setOldVolume] = useState(getVolume()) // useful for toggling mute
   const [paused, setPaused] = useState(isPaused())
+  const [volume, _setVolume] = useState(getVolume())
 
   function isPaused() {
-    const player = element && element.current
-    if (!player) return true
-    return player.paused || player.ended
+    if (!element || !element.current) return true
+    return element.current.paused || element.current.ended
+  }
+
+  function getVolume() {
+    if (!element || !element.current) return 1
+    return element.current.volume
+  }
+
+  function getMuted() {
+    if (!element || !element.current) return false
+    return element.current.muted
   }
 
   function pause() {
-    const player = element && element.current
-    if (!player) return
-    player.pause()
+    if (!element || !element.current) return
+    element.current.pause()
   }
 
   function play() {
-    const player = element && element.current
-    if (!player) return
-    player.play()
+    if (!element || !element.current) return
+    element.current.play()
+  }
+
+  function setVolume(value) {
+    if (!element || !element.current) return
+    setOldVolume(element.current.volume)
+    element.current.volume = value
+
+    // no onmuted event, must set on volumechange
+    if (value === 0) {
+      element.current.muted = true
+    } else {
+      element.current.muted = false
+    }
+    setMuted(element.current.muted)
+  }
+
+  function mute() {
+    if (!element || !element.current) return
+    setVolume(0)
+  }
+
+  function unmute() {
+    if (!element || !element.current) return
+    setVolume(oldVolume)
   }
 
   useEffect(() => {
-    const player = element && element.current
-    const handler = () => setPaused(isPaused())
+    const playPauseHandler = () => setPaused(isPaused())
+    element.current.addEventListener('play', playPauseHandler) // fired by play method or autoplay attribute
+    element.current.addEventListener('playing', playPauseHandler) // fired by resume after being paused due to lack of data
+    element.current.addEventListener('pause', playPauseHandler) // fired by pause method
+    element.current.addEventListener('waiting', playPauseHandler) // fired by pause due to lack of data
 
-    player.addEventListener('play', handler) // fired by play method or autoplay attribute
-    player.addEventListener('playing', handler) // fired by resume after being paused due to lack of data
-    player.addEventListener('pause', handler) // fired by pause method
-    player.addEventListener('waiting', handler) // fired by pause due to lack of data
+    const volumeHandler = () => _setVolume(getVolume())
+    element.current.addEventListener('volumechange', volumeHandler) // fired by a change of volume
 
     return () => {
-      player.removeEventListener('play', handler)
-      player.removeEventListener('playing', handler)
-      player.removeEventListener('pause', handler)
-      player.removeEventListener('waiting', handler)
+      element.current.removeEventListener('play', playPauseHandler)
+      element.current.removeEventListener('playing', playPauseHandler)
+      element.current.removeEventListener('pause', playPauseHandler)
+      element.current.removeEventListener('waiting', playPauseHandler)
+
+      element.current.removeEventListener('volumechange', volumeHandler)
     }
   }, [])
 
   return {
+    mute,
+    muted,
+    unmute,
     pause,
     paused,
     play,
-    toggle: paused ? play : pause
+    setVolume,
+    toggle: paused ? play : pause,
+    volume
   }
 }
