42c4dc7e0c54768fd46f1042eb60ba7d45dd089d
Cian Foley <cian.foley@nearform.com>
2019-02-04 22:13:06 +0000

    improved resize

--- storybook/stories/resize/README.md
+++ storybook/stories/resize/README.md
@@ -1,6 +1,6 @@
 ## useResize Hook
 
-The useResize hook listens for changes to browser window size.  Passing a parameter throttles the rate at which this hook updates.
+The useResize hook listens for changes to browser window size.  Passing an optional 'skip' parameter throttles the rate at which this hook updates based on skipping animation frames.
 
 Import as follows:
 
@@ -16,8 +16,11 @@ const { width, height } = useResize(20)
 ```
 
 Parameters:
-- optional object: set property skip (int) to the amount of animation frames to skip before each update (default: no throttling)
+- options (optional): set { skip } property to the amount of animation frames to skip before each update.
+    - null: no throttling (default)
+    - 0: no frames skipped (as fast as renderAnimationFrame)
+    - 1+: skip one or more frames e.g. 1 = 1/2 the render rate, 20 = 3fps if renderAnimationFrame is working at 60fps
 
 Returns an object containing:
 - width (int), height (int): dimensions of the screen
-- throttled (boolean): if set, throttling is enabled
+- throttled (boolean): whether throttling is enabled or not
---
+++ test/unit/utils/fps.unit.test.js
@@ -0,0 +1,113 @@
+import expect from 'expect'
+
+import { fpsToMs, initRaf, nextRaf, cleanupRaf } from '../../../src/utils/fps'
+import { isNull } from 'util'
+
+describe('fps tests', () => {
+  it('null returns default', () => {
+    expect(fpsToMs(null)).toBe(null)
+  })
+  it('it converts to whole number expected', () => {
+    expect(fpsToMs(1)).toBe(1000)
+    expect(fpsToMs(10)).toBe(100)
+    expect(fpsToMs(30)).toBe(33)
+    expect(fpsToMs(60)).toBe(16)
+    expect(fpsToMs(120)).toBe(8)
+    expect(fpsToMs(1000)).toBe(1)
+  })
+  it('it ensures min and max', () => {
+    expect(fpsToMs(0)).toBe(null)
+    expect(fpsToMs(200000)).toBe(null)
+  })
+  it('invalid returns null', () => {
+    expect(fpsToMs('blaa')).toBe(null)
+    expect(fpsToMs(undefined)).toBe(null)
+    expect(fpsToMs(null)).toBe(null)
+  })
+  it('raf initialisation', () => {
+    let raf = initRaf(3)
+
+    expect(raf).toEqual({
+      skip: 3,
+      ticking: false,
+      tick: 0,
+      rafId: null,
+      lastFrameTimoutId: null,
+      fallBackTimeoutId: null
+    })
+
+    raf = initRaf('5')
+
+    expect(raf).toEqual({
+      skip: 5,
+      ticking: false,
+      tick: 0,
+      rafId: null,
+      lastFrameTimoutId: null,
+      fallBackTimeoutId: null
+    })
+
+    raf = initRaf(0)
+
+    expect(raf).toEqual({
+      skip: 0,
+      ticking: false,
+      tick: 0,
+      rafId: null,
+      lastFrameTimoutId: null,
+      fallBackTimeoutId: null
+    })
+  })
+  it('raf initialisation invalid param', () => {
+    let raf = initRaf(null)
+    expect(raf).toEqual(null)
+
+    raf = initRaf()
+    expect(raf).toEqual(null)
+
+    raf = initRaf('blaa')
+    expect(raf).toEqual(null)
+  })
+  it('next raf', (done) => {
+    let raf = initRaf(2)
+
+    expect(raf).toEqual({
+      skip: 2,
+      ticking: false,
+      tick: 0,
+      rafId: null,
+      lastFrameTimoutId: null,
+      fallBackTimeoutId: null
+    })
+
+    for (let i = 0; i < 3; i++) {
+      nextRaf(raf, () => {
+        expect(raf).toContain({
+          ticking: true,
+          tick: 0
+        })
+        done()
+      })
+      if (i === 3) {
+        expect(raf).toContain({
+          ticking: false,
+          tick: i
+        })
+      }
+    }
+  })
+  it('clear raf', (done) => {
+    let raf = initRaf(0)
+    expect(raf).toContain({
+      rafId: null,
+      ticking: false,
+      tick: 0
+    })
+    nextRaf(raf, () => {
+      expect(raf.rafId).toNotBe(null)
+      cleanupRaf(raf)
+      expect(raf.rafId).toBe(null)
+      done()
+    })
+  })
+})
