02e6928e89b760908330ac3b68daefc24ed1b323
Cian Foley <cian.foley@nearform.com>
2019-02-05 12:12:11 +0000

    simpler test

--- test/acceptance/index.acceptance.test.js
+++ test/acceptance/index.acceptance.test.js
@@ -3,8 +3,6 @@ import { Selector } from 'testcafe'
 fixture('Storybook').page('http://localhost:3000/')
 
 test('Storybook is rendered', async (t) => {
-  await t.switchToIframe('#storybook-preview-iframe')
-
-  const fullScreen = Selector('#fullscreen')
-  await t.expect(fullScreen.exists).ok()
+  const r = Selector('#root')
+  await t.expect(r.exists).ok()
 })
