85546229b363eacd0f9584ec201d80d91897fc34
thomas <thomas.treffry@nearform.com>
2019-02-05 16:49:52 -0800

    Test orientationchange event

--- test/unit/hooks/orientation.unit.test.js
+++ test/unit/hooks/orientation.unit.test.js
@@ -1,3 +1,4 @@
+import { fireEvent, flushEffects } from 'react-testing-library'
 import { testHook, cleanup } from 'react-proxy-hook'
 import { useOrientation } from '../../../src'
 
@@ -7,8 +8,30 @@ describe('useOrientation', () => {
   it('sets initial state to window.screen.orientation', () => {
     let angle, type
     window.screen.orientation = { angle: 0, type: 'portrait-primary' }
+
     testHook(() => ({ angle, type } = useOrientation()))
+
     expect(angle).toBe(0)
     expect(type).toBe('portrait-primary')
+  })
+
+  it('updates state on "orientationchange"', () => {
+    let angle, type
+    window.screen.orientation = { angle: 0, type: 'portrait-primary' }
+    testHook(() => ({ angle, type } = useOrientation()))
+
+    flushEffects()
+
+    window.screen.orientation = { angle: 90, type: 'landscape-primary' }
+    fireEvent(
+      window,
+      new Event('orientationchange', {
+        bubbles: false,
+        cancelable: false
+      })
+    )
+
+    expect(angle).toBe(90)
+    expect(type).toBe('landscape-primary')
   })
 })
