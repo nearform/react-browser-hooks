bcc125fbf0d7ba0350583631c92724074ec3d251
Simone Busoli <simone.busoli@gmail.com>
2019-02-07 12:31:25 +0100

    Use jest-specific babel config to allow nwb to build

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 // this file can be removed when we get rid of acceptance tests
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', 'test/acceptance'],
-  setupFiles: ['<rootDir>/test/unit/setup.js']
+  setupFiles: ['<rootDir>/test/unit/setup.js'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/jest.transform.js'
+  }
 }
