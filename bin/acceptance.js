fe175febb330e8cda5f3d203527d8e704c40d346
thomas <thomas.treffry@nearform.com>
2019-02-11 11:59:00 -0800

    Close tests on end

--- bin/acceptance.js
+++ bin/acceptance.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
-const Shell = require('shelljs')
+const ChildProcess = require('child_process')
 
-const app = Shell.exec('npm start', { async: true })
-const tests = Shell.exec('npm run acceptance', () => {
-  app.kill('SIGINT')
+const app = ChildProcess.spawn('npm', ['start'], {
+  detach: true,
+  stdio: 'inherit'
+})
+
+const test = ChildProcess.spawn('npm', ['run', 'acceptance'], {
+  detach: true,
+  stdio: 'inherit'
+})
+
+app.on('error', (err) => {
+  console.log('Failed to start subprocess:', err)
+  test.kill()
+})
+
+test.on('close', () => {
+  app.kill()
 })
