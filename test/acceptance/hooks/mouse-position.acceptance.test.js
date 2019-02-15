7556084f82810a9ac07a34a6355da8186ce5d467
thomas <thomas.treffry@nearform.com>
2019-02-15 12:43:26 -0800

    Refactor selectors into a page model

--- test/acceptance/hooks/mouse-position.acceptance.test.js
+++ test/acceptance/hooks/mouse-position.acceptance.test.js
@@ -1,27 +1,28 @@
-import { Selector } from 'testcafe'
 import globals from '../globals'
+import Storybook from '../storybook'
+
+const storybook = new Storybook()
 
 fixture('Mouse Position Hook')
   .page(
     globals.url +
       '/?selectedKind=Mouse Position&selectedStory=Default&full=0&addons=1&stories=1&panelRight=1&addonPanel=REACT_STORYBOOK%2Freadme%2Fpanel'
   )
-  .beforeEach((t) => t.switchToIframe('iframe#storybook-preview-iframe'))
+  .beforeEach((t) => t.switchToIframe(storybook.iframe))
   .afterEach((t) => t.switchToMainWindow())
 
 test('The mouse position demo is rendered', async (t) => {
-  const title = Selector('.mouse-position-demo').child('h2')
+  const { title } = storybook.hooks.mousePosition
   await t.expect(title.textContent).contains('Mouse Position Demo')
 })
 
 test('The mouse position defaults to 0,0', async (t) => {
-  const description = Selector('.mouse-position-demo').child('p')
+  const { description } = storybook.hooks.mousePosition
   await t.expect(description.textContent).contains('X: 0px, Y: 0px')
 })
 
 test('The mouse position updates when the mouse moves', async (t) => {
-  const html = Selector('html')
-  const description = Selector('.mouse-position-demo').child('p')
+  const { description, html } = storybook.hooks.mousePosition
 
   await t.hover(html)
 
