360c13bc467c68f0c5826fb8538e5151ed58cba1
thomas <thomas.treffry@nearform.com>
2019-02-14 19:09:00 -0800

    Add mouse position acceptance tests

---
+++ test/acceptance/hooks/mouse-position.acceptance.test.js
@@ -0,0 +1,43 @@
+import { act, Selector } from 'testcafe'
+import globals from '../globals'
+
+fixture('Mouse Position Hook').page(
+  globals.url +
+    '/?selectedKind=Mouse Position&selectedStory=Default&full=0&addons=1&stories=1&panelRight=1&addonPanel=REACT_STORYBOOK%2Freadme%2Fpanel'
+)
+
+test('The mouse position demo is rendered', async (t) => {
+  const title = Selector('.mouse-position-demo').child('h2')
+
+  await t
+    .switchToIframe('iframe#storybook-preview-iframe')
+    .expect(title.textContent)
+    .contains('Mouse Position Demo')
+    .switchToMainWindow()
+})
+
+test('The mouse position defaults to 0,0', async (t) => {
+  const description = Selector('.mouse-position-demo').child('p')
+
+  await t
+    .switchToIframe('iframe#storybook-preview-iframe')
+    .expect(description.textContent)
+    .contains('X: 0px, Y: 0px')
+    .switchToMainWindow()
+})
+
+test('The mouse position updates when the mouse moves', async (t) => {
+  const html = Selector('html')
+  const description = Selector('.mouse-position-demo').child('p')
+
+  await t.switchToIframe('iframe#storybook-preview-iframe').hover(html)
+
+  const dimensions = await description.textContent
+  const numbers = /\d+/g
+  const [x, y] = dimensions.match(numbers)
+
+  await t
+    .expect(description.textContent)
+    .contains(`X: ${x}px, Y: ${y}px`)
+    .switchToMainWindow()
+})
