b329093a5a7dc45360e81eff26087c3bfb0eb2f1
thomas <thomas.treffry@nearform.com>
2019-02-15 12:06:03 -0800

    Refactor iframe handling

--- test/acceptance/hooks/mouse-position.acceptance.test.js
+++ test/acceptance/hooks/mouse-position.acceptance.test.js
@@ -1,36 +1,29 @@
 import { Selector } from 'testcafe'
 import globals from '../globals'
 
-fixture('Mouse Position Hook').page(
-  globals.url +
-    '/?selectedKind=Mouse Position&selectedStory=Default&full=0&addons=1&stories=1&panelRight=1&addonPanel=REACT_STORYBOOK%2Freadme%2Fpanel'
-)
+fixture('Mouse Position Hook')
+  .page(
+    globals.url +
+      '/?selectedKind=Mouse Position&selectedStory=Default&full=0&addons=1&stories=1&panelRight=1&addonPanel=REACT_STORYBOOK%2Freadme%2Fpanel'
+  )
+  .beforeEach((t) => t.switchToIframe('iframe#storybook-preview-iframe'))
+  .afterEach((t) => t.switchToMainWindow())
 
 test('The mouse position demo is rendered', async (t) => {
   const title = Selector('.mouse-position-demo').child('h2')
-
-  await t
-    .switchToIframe('iframe#storybook-preview-iframe')
-    .expect(title.textContent)
-    .contains('Mouse Position Demo')
-    .switchToMainWindow()
+  await t.expect(title.textContent).contains('Mouse Position Demo')
 })
 
 test('The mouse position defaults to 0,0', async (t) => {
   const description = Selector('.mouse-position-demo').child('p')
-
-  await t
-    .switchToIframe('iframe#storybook-preview-iframe')
-    .expect(description.textContent)
-    .contains('X: 0px, Y: 0px')
-    .switchToMainWindow()
+  await t.expect(description.textContent).contains('X: 0px, Y: 0px')
 })
 
 test('The mouse position updates when the mouse moves', async (t) => {
   const html = Selector('html')
   const description = Selector('.mouse-position-demo').child('p')
 
-  await t.switchToIframe('iframe#storybook-preview-iframe').hover(html)
+  await t.hover(html)
 
   const dimensions = await description.textContent
   const numbers = /\d+/g
@@ -45,5 +38,4 @@ test('The mouse position updates when the mouse moves', async (t) => {
     .eql(expectedX)
     .expect(parseInt(y))
     .eql(expectedY)
-    .switchToMainWindow()
 })
